9a6630d42682bac8618e237be706fab3cc371cae
Sami <102156149+55d41251@users.noreply.github.com>
2026-03-06 09:32:01 +0200

    message

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = ({ isAuthenticated, setIsAuthenticated }) => {
   return (
     <nav className="navbar">
       <Link to="/">
-        <h1>React Jobs</h1>
+        <h1>React Jobs 2027</h1>
       </Link>
       <div className="links">
         {isAuthenticated && (
